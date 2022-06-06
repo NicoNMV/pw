56ee138d1f6efa1fd3fb7aa2a13a5bc43d933d35
NMV <98959079+NicoNMV@users.noreply.github.com>
2022-06-06 07:10:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+/* retorna o primaiero elemento que corresponde 
+aos seletores específicos*/
+
+let $TituloPagina = document.querySelector("#titulo");
+
+/* a propriedade text.content 
+define ou retorna o conteúdo de texto*/
+
+$TituloPagina.textContent = "ETEC Professor Basiledes de Godoy";
+
+let $tabela = document.querySelector("#tabela-aluno");
+// console.log($tabela);
+
+let $RegistroAlunos = document.querySelectorAll(".aluno");
+console.log($RegistroAlunos);
+
+for(let j=0; j<$RegistroAlunos.length; j++){
+    let $contagem = $RegistroAlunos[j];
+    let $nome = $contagem.querySelector(".td-nome").textContent;
+    let $n1 = $contagem.querySelector(".td-n1").textContent;
+    let $n2 = $contagem.querySelector(".td-n2").textContent;
+    let $n3 = $contagem.querySelector(".td-n3").textContent;
+
+    let $mediaFinal = $calcularMedia($n1, $n2, $n3);
+
+    let $media = $contagem.querySelector(".td-media");
+    $media.textContent = $mediaFinal.toFixed(1);
+
+    if($media.textContent < 6){
+        $media.style.color = "red";
+        $media.style.fontWeight = "bold";
+    }
+
+    else{
+        $media.style.color = "blue";
+        $media.style.fontWeight = "bold";
+    }
+
+    let $situacao = $contagem.querySelector(".td-situacao");
+    $situacao.textContent = mostrarSituacao($mediaFinal)[0];
+    $situacao.classList.add(mostrarSituacao($mediaFinal)[1]);
+
+    
+
+}
+
+function $calcularMedia(x, y, z){
+    return(parseFloat(x) + parseFloat(y) + parseFloat(z)) / 3;
+}
+
+function mostrarSituacao(x){
+    let $resultado = [];
+
+    if(x < 6){
+        $resultado.push("Reprovado");
+        $resultado.push("reprovado");
+    }
+
+    else{
+        $resultado.push("Aprovado");
+        $resultado.push("aprovado");
+    }
+
+    return $resultado;
+}
---
+++ scriptform.js
@@ -0,0 +1,56 @@
+const botao = document.querySelector("#adicionar-aluno");
+
+botao.addEventListener("click", function(){
+    event.preventDefault();
+
+    let $dadosform = document.querySelector("#form-aluno");
+    let $dadosAluno = novoAluno($dadosform);
+    
+    console.log($dadosAluno.vNome);
+    console.log($dadosAluno.vN1);
+});
+
+function novoAluno(formulario){
+    if(formulario.querySelector("#nome").value == ""){
+        alert("Obrigatório informar o campo nome");
+        formulario.querySelector("#nome").focus();
+    }
+    else if(formulario.querySelector("#nome").value.lenght < 8){
+        alert("Obrigatório informar o nome completo do aluno.");
+        formulario.querySelector("#nome").focus();
+    }
+    else if(formulario.querySelector("#n1").value == ""){
+        alert("Obrigatório informar a primeira nota do aluno.");
+        formulario.querySelector("#n1").focus();
+    }
+    else if(isNaN(parseFloat(formulario.querySelector("#n1").value))){
+        alert("Inserir apenas valores númericos no campo nota 1.");
+        formulario.querySelector("#n1").value == "";
+        formulario.querySelector("#n1").focus();
+    }
+    else if(formulario.querySelector("#n2").value == ""){
+        alert("Obrigatório informar a segunda nota do aluno.");
+        formulario.querySelector("#n2").focus();
+    }
+    else if(isNaN(parseFloat(formulario.querySelector("#n2").value))){
+        alert("Inserir apenas valores númericos no campo nota 2.");
+        formulario.querySelector("#n2").value == "";
+        formulario.querySelector("#n2").focus();
+    }
+    else if(formulario.querySelector("#n3").value == ""){
+        alert("Obrigatório informar a terceira nota do aluno.");
+        formulario.querySelector("#n3").focus();
+    }
+    else if(isNaN(parseFloat(formulario.querySelector("#n3").value))){
+        alert("Inserir apenas valores númericos no campo nota 3.");
+        formulario.querySelector("#n3").value == "";
+        formulario.querySelector("#n3").focus();
+    }
+    else{
+        let $vetorAluno = {
+            vNome:formulario.querySelector("#nome").value,
+            vN1:formulario.querySelector("#n1").value.replace(",",",")
+        }
+        return $vetorAluno;
+    }
+};
